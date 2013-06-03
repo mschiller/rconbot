6e0be1df076e595cb2cf2605a7e9a95434cbbcf0
schubert <cardozoschubert@gmail.com>
2013-06-03 17:54:57 +0530

    modularize

--- parser.rb
+++ parser.rb
@@ -1,3 +1,7 @@
+require 'redis'
+require 'rcon'
+
+
 TIMESTAMP_REGEX = "L 0?[0-9]\/[0-9]{2}\/[0-9]{4} - [0-9]{2}:[0-9]{2}:[0-9]{2}: "
 
 KILL_REGEX = /^#{TIMESTAMP_REGEX}\"(.+)<[0-9]+><(STEAM_[0-5]:[0-1]:[0-9]+)><(CT|TERRORIST)>\" killed \"(.+)<[0-9]+><(STEAM_[0-5]:[0-1]:[0-9]+)><(CT|TERRORIST)>\" with \"([a-z0-9]*)\"$/
@@ -8,28 +12,76 @@ ATTACK_REGEX = /^#{TIMESTAMP_REGEX}\"(.+)<[0-9]+><(STEAM_[0-5]:[0-1]:[0-9]+)><(C
 
 LIVE_REGEX = /^#{TIMESTAMP_REGEX}Rcon: \"rcon [0-9]* \".*\" exec live.cfg" from \"[0-9\.:]*\"/
 
+CHANGEMAP_REGEX = /changelevel/
+
 ROUNDEND_REGEX = /^#{TIMESTAMP_REGEX}Team \"(CT|TERRORIST)\" triggered \"(Target_Bombed|Target_Saved|Bomb_Defused|CTs_Win|Terrorists_Win)\" \(CT \"([0-9]{1,2})\"\) \(T "([0-9]{1,2})"\)/
 
 
 module RConBot
 
-  class Match
-    attr_reader :max_rounds, :team_size
-    attr_accessor :half
+  class Stats
+    def initialize(team1, team2, map)
+      @stats_key_prefix = "#{team1}:#{team2}:#{map}"
+    end
+
+    # type weapon, ct, t, or nil (overall)
+    def skill(type = nil)
+      
+    end
+
+    def kills(steam_id)
+      
+    end
+
+    def deaths(steam_id)
+      
+    end
+
+    def kd_ratio(steam_id)
+      
+    end
     
-    def initialize
+  end
+
+  class Match
+    attr_reader :max_rounds, :team_size, :result, :team1, :team2, :alive, :stats, :status
+    attr_accessor :half, :score, :result
+    
+    def initialize(team1 = '1', team2 = '2', map = 'dust2')
       @live = false
-      @half = 1
+      @half = 0
       @team_size = 5
       @max_rounds = 15
+      @team1 = team1
+      @team2 = team2
+      @score = [[0, 0], [0, 0]]
+      @stats = Stats.new(team1, team2, map)
+      @status = :wait_on_join # :wait_on_join :first_warmup :first_half :second_warmup :second_half :finished
+    end
+
+    def teams
+      [@team1, @team2]
     end
     
     def start
       @live = true
+      next_round
+    end
+    
+    def next_round
+      @alive = {'CT' => @team_size, 'TERRORIST' => @team_size}
     end
 
     def stop
       @live = false
+    end
+    
+    def round
+      @score.flatten.inject(0){|s, i| s += i; s}
+    end
+
+    def team_score(team)
+      @score[0][team] + @score[1][team]
     end
 
     def live?
@@ -39,39 +91,71 @@ module RConBot
     def map
       @map
     end
+
+    def stats
+      
+    end
     
     def log_filename
       './logs/test.log'
     end
+
+  end
+
+  class PlayerStats
+    attr_accessor :redis_connection
+
   end
 
   class Bot
     attr_accessor :match
+    attr_reader :rcon_connection
     
-    def connect
-      @match = Match.new
-      filename =  @match.log_filename
-      f = File.open(filename, "r")
-      f.seek(0, IO::SEEK_END)
-      while true do
-        select([f])
-        line = f.gets
-        process_line(line)
+    # options :team1 :team2 :maps :timelimit
+    def connect(host, port, password, options = {})
+      @rcon_connection = RCon::Query::Original.new(host, port, password)
+      @rcon_connection.command("sv_password #{options[:sv_password]}")
+      options[:maps].each do |map|
+        @match = Match.new(options[:team1], options[:team2], map)
+        @rcon_connection.command("kick all 'Sorry, scheduled match to take place. Visit www.fragg.in to participate.'")
+        @rcon_connection.command("changelevel #{map}")
+        filename =  @match.log_filename
+        f = File.open(filename, "r")
+        f.seek(0, IO::SEEK_END)
+        while true do
+          select([f])
+          line = f.gets
+          wait(line, @match.status)
+        end
       end
     end
-    
+
+    def wait(line, status)
+      case status
+      when :wait_on_join
+        @match.status = :first_warmup if line =~ /connected/
+      when :first_warmup
+        @match.status = :first_half if line =~ /say ready/
+      when :second_warmup
+        @match.status = :second_half if line =~ /say ready/
+      when :first_half, :second_half
+        process_line(line)
+      when 
+      end
+    end
+
     def process_line(line)
-      if m = LIVE_REGEX.match(line) or  m = /sv_restart/.match(line)
-        puts "RESET!!!"
-        # $redis.flushdb if $half == 1
-        puts "LIVE!!! HALF => #{@match.half}"
+      if !@match.result and (m = LIVE_REGEX.match(line) or  m = /sv_restart/.match(line)) # to be removed for rconbot (strict)
+        puts "LIVE!!! HALF => #{@match.half + 1}"
         @match.start
+      # elsif m = CHANGEMAP_REGEX.match(line)
+      #   # @match = Match.new('a', 'b', 'inferno')
       elsif @match.live? and m = KILL_REGEX.match(line)
         t, k_name, k_steam_id, k_team, v_name, v_steam_id, v_team, weapon = m.to_a
-        
         raise l if k_steam_id == v_steam_id # can happen in suicide
         
-        $alive[v_team] -= 1
+        @match.alive[v_team] -= 1
+
         $healths[v_steam_id] ||= 100
         
         # add aliases in case of change
@@ -79,21 +163,22 @@ module RConBot
         $redis.zincrby("alias:#{v_steam_id}", 1, v_name)
         
         # kills
-        k = $redis.incr("kills:#{k_steam_id}")
+        k = $redis.incr("kills:#{k_steam_id}") #unless k_team == v_team # friendly fire
         # deaths
-        d = $redis.incr("deaths:#{v_steam_id}")
+        d = $redis.incr("deaths:#{v_steam_id}") #unless
         
         # points for killer
-        points = ((@match.team_size - $alive[v_team]) + (@match.team_size - $alive[k_team]))
+        points = ((@match.team_size - @match.alive[k_team]) + (@match.team_size - @match.alive[v_team]))
+        points = 0 if k_team == v_team # friendly fire
         
         case k_team
         when 'CT'
-          pct = $redis.zincrby("skill.ct", points, k_steam_id)
+          $redis.zincrby("skill.ct", points, k_steam_id)
         when 'TERRORIST'
-          pt = $redis.zincrby("skill.t", points, k_steam_id)
+          $redis.zincrby("skill.t", points, k_steam_id)
         end
         
-        # puts " -- #{t.split('-').last} #{k_name[0..4]} (#{k_team}) killed #{v_name[0..4]} (#{v_team})"
+        puts " -- #{k_name[0..4]} (#{k_team[0]}) killed #{v_name[0..4]} (#{v_team[0]}) with #{weapon} -- #{points} #{'wtf' if k_team == v_team} #{@match.alive.inspect}"
         
         $redis.zincrby("skill.#{weapon}", points, k_steam_id)
         $redis.zincrby("weapon.usage", 1, weapon)
@@ -109,13 +194,34 @@ module RConBot
         #   $healths[t_steam_id] = health
       elsif @match.live? and m = ROUNDEND_REGEX.match(line)
         t, winner, reason, ct_score, t_score = m.to_a
-        puts "#{'*' * 25}  HALF #{@match.half}; ROUND #{ct_score.to_i+t_score.to_i}; CT: #{ct_score} T: #{t_score} #{'*' * 25}"
-        if ct_score.to_i + t_score.to_i == @match.max_rounds
-          @match.stop
-          @match.half = 2
+
+        if @match.half == 0
+          @match.score[@match.half][0] = ct_score.to_i
+          @match.score[@match.half][1] = t_score.to_i
+        elsif @match.half == 1
+          @match.score[@match.half][1] = ct_score.to_i
+          @match.score[@match.half][0] = t_score.to_i
         end
-        $alive['CT'] = @match.team_size
-        $alive['TERRORIST'] = @match.team_size
+
+        puts "HALF #{@match.half + 1}, ROUND #{@match.round}, SCORE #{@match.team1} => #{@match.score[@match.half][0]}, #{@match.team2} => #{@match.score[@match.half][1]} #{reason}"
+
+        if @match.team_score(0) == 16 
+          @match.stop
+          @match.result = 0 # CT
+        elsif @match.team_score(1) == 16
+          @match.stop 
+          @match.result = 1 # T
+        elsif @match.round == @match.max_rounds
+          @match.stop
+          @match.half += 1
+        elsif @match.round == @match.max_rounds * 2
+          @match.stop
+          @match.result = -1 # Draw
+        end
+
+        puts "RESULT => #{@match.teams[@match.result]}" if @match.result
+
+        @match.next_round
         $healths = {}
       end
     end
@@ -129,8 +235,6 @@ end
 `rm logs/test.log`
 `touch logs/test.log`
 
-require 'redis'
-# require 'rcon'
 
 # $rcon = RCon::Query::Original.new("schubert.com", '27015', 'tuesdaysgone')
 
@@ -140,38 +244,39 @@ $redis.flushdb
 puts "-" * 102
 
 
-$alive = {'CT' => $team_size, 'TERRORIST' => $team_size}
+$alive = {'CT' => 5, 'TERRORIST' => 5}
 $healths = {} # keeps track of player health, if does not exist 100 is assumed
 
 def stats
   puts '*' * 100
   
   puts "\nSKILL CT\n"
-  puts $redis.zrevrange("skill.ct", 0, -1, :with_scores => true).map{|x| {$redis.zrevrange("alias:#{x[0]}",0,-1)[0] => x[1] }}
+  puts $redis.zrevrange("skill.ct", 0, -1, :with_scores => true).map{|x| {$redis.zrevrange("alias:#{x[0]}",0,1)[0] => x[1] }}
   puts "SKILL T"
-  puts $redis.zrevrange("skill.t", 0, -1, :with_scores => true).map{|x| {$redis.zrevrange("alias:#{x[0]}",0,-1)[0] => x[1] }}
+  puts $redis.zrevrange("skill.t", 0, -1, :with_scores => true).map{|x| {$redis.zrevrange("alias:#{x[0]}",0,1)[0] => x[1] }}
   
   puts '*' * 100
   
   puts "\nSKILL\n"
   $redis.zunionstore("skill", ["skill.ct", "skill.t"])
-  puts $redis.zrevrange("skill", 0, -1, :with_scores => true).map{|x| {$redis.zrevrange("alias:#{x[0]}",0,-1)[0] => x[1] }}
+  puts $redis.zrevrange("skill", 0, -1, :with_scores => true).map{|x| {$redis.zrevrange("alias:#{x[0]}",0,1)[0] => x[1] }}
   
-  puts '*' * 100
+  # puts '*' * 100
   
-  $redis.zrevrange("weapon.usage", 0, -1).each do |weapon|
-    puts "\nSKILL BY WEAPON #{weapon}\n"
-    puts $redis.zrevrange("skill.#{weapon}", 0, -1, :with_scores => true).map{|x| {$redis.zrevrange("alias:#{x[0]}",0,-1)[0] => x[1] }}  
-  end
+  # $redis.zrevrange("weapon.usage", 0, -1).each do |weapon|
+  #   puts "\nSKILL BY WEAPON #{weapon}\n"
+  #   puts $redis.zrevrange("skill.#{weapon}", 0, -1, :with_scores => true).map{|x| {$redis.zrevrange("alias:#{x[0]}",0,-1)[0] => x[1] }}  
+  # end
+
+  # puts '*' * 100
+  
+  # puts "\nWEAPONS USED\n"
+  # puts $redis.zrevrange("weapon.usage", 0, -1, :with_scores => true).map{|x| {x[0] => x[1]}}
   
   puts '*' * 100
   
   puts "\nK:D RATIO\n"
-  puts $redis.zrevrange("ratio", 0, -1, :with_scores => true).map{|x| {$redis.zrevrange("alias:#{x[0]}",0,-1)[0] => x[1] }}
+  puts $redis.zrevrange("ratio", 0, -1, :with_scores => true).map{|x| {$redis.zrevrange("alias:#{x[0]}",0,1)[0] => x[1] }}
   
-  puts '*' * 100
-  
-  puts "\nWEAPONS USED\n"
-  puts $redis.zrevrange("weapon.usage", 0, -1, :with_scores => true).map{|x| {x[0] => x[1]}}
 end
 
