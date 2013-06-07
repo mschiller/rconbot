19d050911b9b61af65afe7dc44137886189f5e32
schubert <cardozoschubert@gmail.com>
2013-06-07 13:52:32 +0530

    slight chanages (10 sec tiemout)

--- lib/rconbot/rcon_connection.rb
+++ lib/rconbot/rcon_connection.rb
@@ -10,14 +10,14 @@ module RconBot
     end
     
     def get_challenge_id
-      response = socket_request("\xFF" * 4 + "challenge rcon" + "\x00", true)
+      response = socket_request("\xFF" * 4 + "challenge rcon" + "\x00")
       if challenge_id = /challenge rcon (\d+)/.match(response)
         @challenge_id = challenge_id[1]
       end
     end
     
     def command(request)
-      socket_request("\xFF" * 4 + "rcon #{@challenge_id} \"#{@password}\" #{request}" + "\x00", true)
+      socket_request("\xFF" * 4 + "rcon #{@challenge_id} \"#{@password}\" #{request}" + "\x00")
     end
     
     def connect
@@ -25,16 +25,15 @@ module RconBot
       @socket.connect(@host, @port)
     end
 
-    def socket_request(request, challenge = false)
+    def socket_request(request)
       puts "=> #{request.inspect}"
       @socket.print(request)
       retval = []
       loop do 
-        select([@socket], nil, nil, 1)
+        select([@socket], nil, nil, 10)
         str = @socket.recv(65507)
         puts "<= #{str.inspect}"
         bytes = str.unpack("c*")
-        puts str[(challenge ? 4 : 5)..-2]
         retval += bytes
         break
       end
